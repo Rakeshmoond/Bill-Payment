bef9277afafd54d7845f17f2d1acd6d7097eb849
Rakeshmoond <2019uce1596@mnit.ac.in>
2021-07-09 21:18:27 +0530

    my website

--- marksheetcreator.js
+++
@@ -1,9 +0,0 @@
-function myfunction(){
-    var x=document.getElementById("name");
-    x.value=x.value.toUpperCase();
-}
-function GFG_Fun()
-{
-    document.getElementById("button").innerHTML="my text"
-}
-document.getElementById("GFG_DOWN").innerHTML="my text"
